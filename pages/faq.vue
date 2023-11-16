83e363663c0c20c25cae281c282035425cbb66ff
ropodl <sarox14@gmail.com>
2023-11-16 21:31:00 +0545

    Update faq.vue

--- pages/faq.vue
+++ pages/faq.vue
@@ -15,13 +15,19 @@ onMounted(() => {
 });
 
 const faqs = ref([]);
+const loading = ref(true);
 const getAllFaqs = async () => {
+  loading.value = true;
   const { data, error } = await useFetch(
     runtimeConfig.public.api_url + "/frontend/faqs"
   );
-  if (error.value) return console.log(error.value);
+  if (error.value) {
+    loading.value = false;
+    return console.log(error.value);
+  }
   console.log(data.value);
   faqs.value = data.value;
+  loading.value = false;
 };
 </script>
 <template>
@@ -40,13 +46,23 @@ const getAllFaqs = async () => {
   <v-container>
     <v-row justify="center">
       <v-col cols="12" md="8">
-        <v-expansion-panels multiple>
-          <v-expansion-panel
-            v-for="(faq, i) in faqs"
-            :title="faq.title"
-            :text="faq.content"
-          ></v-expansion-panel>
-        </v-expansion-panels>
+        <template v-if="loading">
+          <v-skeleton-loader type="article"> </v-skeleton-loader>
+        </template>
+        <template v-else>
+          <template v-if="faqs.length">
+            <v-expansion-panels multiple>
+              <v-expansion-panel
+                v-for="(faq, i) in faqs"
+                :title="faq.title"
+                :text="faq.content"
+              ></v-expansion-panel>
+            </v-expansion-panels>
+          </template>
+          <template v-else>
+            <LazySharedEmpty />
+          </template>
+        </template>
       </v-col>
     </v-row>
   </v-container>

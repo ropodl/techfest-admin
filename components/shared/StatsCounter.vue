65a8cfe30720732aa3bc80df35d992053cdf0a40
ropodl <sarox14@gmail.com>
2023-11-25 05:26:34 +0545

    Update StatsCounter.vue

--- components/shared/StatsCounter.vue
+++ components/shared/StatsCounter.vue
@@ -1,7 +1,6 @@
 <script setup>
 import CountUp from "vue-countup-v3";
 import { useDisplay } from "vuetify";
-const { smAndDown, mdAndDown } = useDisplay();
 
 const stats = [
   {
@@ -32,26 +31,74 @@ const countDownOptions = {
   <section class="position-relative" style="background-color: #1e1e1e">
     <v-container>
       <v-row class="py-16" justify="center">
-        <template v-for="(stat, i) in stats">
-          <v-col cols="12" md="3">
-            <div class="d-flex h-100 w-100 justify-center align-center">
-              <div class="text-center">
-                <v-card-title class="text-h1 d-flex align-start">
-                  <ClientOnly>
-                    <count-up
-                      :options="countDownOptions"
-                      :end-val="stat.number"
-                    ></count-up>
-                  </ClientOnly>
-                  <span class="text-h4 font-weight-bold text-primary">+</span>
-                </v-card-title>
-                <v-card-text class="text-uppercase">
-                  {{ stat.name }}
-                </v-card-text>
-              </div>
+        <v-col cols="12" sm="6" md="3">
+          <div class="d-flex h-100 w-100 justify-center align-center">
+            <div class="text-center">
+              <v-card-title class="text-h1 d-flex align-start">
+                <ClientOnly>
+                  <count-up
+                    :options="countDownOptions"
+                    :end-val="21"
+                  ></count-up>
+                </ClientOnly>
+                <span class="text-h4 font-weight-bold text-primary">+</span>
+              </v-card-title>
+              <v-card-text class="text-uppercase">
+                skilled speakers
+              </v-card-text>
             </div>
-          </v-col>
-        </template>
+          </div>
+        </v-col>
+        <v-col cols="12" sm="6" md="4">
+          <div class="d-flex h-100 w-100 justify-center align-center">
+            <div class="text-center">
+              <v-card-title class="text-h1 d-flex align-start">
+                <ClientOnly>
+                  <count-up
+                    :options="countDownOptions"
+                    :end-val="15000"
+                  ></count-up>
+                </ClientOnly>
+                <span class="text-h4 font-weight-bold text-primary">+</span>
+              </v-card-title>
+              <v-card-text class="text-uppercase">
+                expected visitors
+              </v-card-text>
+            </div>
+          </div>
+        </v-col>
+        <v-col cols="12" sm="6" md="2">
+          <div class="d-flex h-100 w-100 justify-center align-center">
+            <div class="text-center">
+              <v-card-title class="text-h1 d-flex align-start">
+                <ClientOnly>
+                  <count-up
+                    :options="countDownOptions"
+                    :end-val="42"
+                  ></count-up>
+                </ClientOnly>
+                <span class="text-h4 font-weight-bold text-primary">+</span>
+              </v-card-title>
+              <v-card-text class="text-uppercase">Mega Events</v-card-text>
+            </div>
+          </div>
+        </v-col>
+        <v-col cols="12" sm="6" md="3">
+          <div class="d-flex h-100 w-100 justify-center align-center">
+            <div class="text-center">
+              <v-card-title class="text-h1 d-flex align-start">
+                <ClientOnly>
+                  <count-up
+                    :options="countDownOptions"
+                    :end-val="17"
+                  ></count-up>
+                </ClientOnly>
+                <span class="text-h4 font-weight-bold text-primary">+</span>
+              </v-card-title>
+              <v-card-text class="text-uppercase">Pre Events</v-card-text>
+            </div>
+          </div>
+        </v-col>
       </v-row>
     </v-container>
   </section>
